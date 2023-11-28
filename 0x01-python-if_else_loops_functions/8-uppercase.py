81dd50cddb389671bc3773354c89cc73bfcb54e5
jeonsho <jeonsho1999@gmail.com>
2023-11-28 07:06:13 -0800

    test

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if 97 <= ord(str[i]) <= 122:
+            print(chr(ord(str[i]) - 32), end="")
+        else:
+            print(str[i], end="")
+    print("")

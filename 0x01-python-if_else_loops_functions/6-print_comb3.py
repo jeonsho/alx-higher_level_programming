126ee48e0cee867c7e24570c366391600390a367
jeonsho <jeonsho1999@gmail.com>
2023-11-28 06:49:42 -0800

    test

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if i < 9 and j < 9:
-            print("{:02d}, ".format(i * 10 + j), end="")
-        else:
-            print("{:02d}".format(i * 10 + j))
+        print("{}{}".format(i, j), end=", ")
+print("")

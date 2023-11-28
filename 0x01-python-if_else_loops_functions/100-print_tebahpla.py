a59678dc31b66b5a687551541387e3c9c28dd3c5
jeonsho <jeonsho1999@gmail.com>
2023-11-28 08:50:47 -0800

    test

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,8 +3,9 @@ def print_reverse_alphabet():
     for ascii_code in range(ord('z'), ord('A') - 1, -1):
         letter = chr(ascii_code)
         if 'a' <= letter <= 'z':
-            print("{}".format(letter.upper()), end='')
+            print("{}".format(letter), end='')
         else:
             print("{}".format(letter.lower()), end='')
 
 print_reverse_alphabet()
+

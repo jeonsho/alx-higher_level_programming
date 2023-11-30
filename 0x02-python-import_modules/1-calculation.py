23b23ccfc8a36c0f5e259c6b7929232c1eab6b68
jeonsho <jeonsho1999@gmail.com>
2023-11-30 04:04:45 -0800

    test

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,11 +4,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    result_add = add(a, b)
-    result_subtract = subtract(a, b)
-    result_multiply = multiply(a, b)
-    result_divide = divide(a, b)
-
     print("Addition: {}".format(result_add))
     print("Subtraction: {}".format(result_subtract))
     print("Multiplication: {}".format(result_multiply))

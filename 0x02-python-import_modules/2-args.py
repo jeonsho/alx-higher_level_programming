44ab4e7003a9799f28ff5098bba4f86053917182
jeonsho <jeonsho1999@gmail.com>
2023-11-30 06:01:52 -0800

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    num_args = len(argv)
-    if num_args == 0:
-        print("0 argument.")
-        print(".")
-    else:
-        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
-        for i, arg in enumerate(argv, start=1):
-            print(f"{i}: {arg}")
+    num_args = len(argv) - 1  # Subtract 1 to exclude the script name itself
+
+    print(
+        "Number of argument{}: {}".format("s" if num_args != 1 else "", num_args),
+        end=""
+    )
+    
+    print(
+        "{}".format("s" if num_args != 1 else ":") if num_args != 0 else ".",
+        end="\n\n"
+    )
+
+    if num_args > 0:
+        for i in range(num_args):
+            print("{}: {}".format(i + 1, argv[i + 1]))

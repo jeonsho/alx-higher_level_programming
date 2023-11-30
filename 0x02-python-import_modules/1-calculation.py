a349dcee8d30eecd23598be08e6ea386f9849b03
jeonsho <jeonsho1999@gmail.com>
2023-11-30 04:01:13 -0800

    test

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, subtract, multiply, divide
+    a = 10
+    b = 5
+
+    result_add = add(a, b)
+    result_subtract = subtract(a, b)
+    result_multiply = multiply(a, b)
+    result_divide = divide(a, b)
+
+    print("Addition: {}".format(result_add))
+    print("Subtraction: {}".format(result_subtract))
+    print("Multiplication: {}".format(result_multiply))
+    print("Division: {}".format(result_divide))

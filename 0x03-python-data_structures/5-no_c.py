6ea7447b45b66c6fb826623f65ee68d880553242
jeonsho <jeonsho1999@gmail.com>
2023-12-03 04:29:21 -0800

    test

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result_string = ''.join(char for char in my_string if char.lower() not in {'c'})
+    filt_chars = (char for char in my_string if char.lower() not in {'c', 'C'})
+    result_string = ''.join(filtered_chars)
 
     return result_string

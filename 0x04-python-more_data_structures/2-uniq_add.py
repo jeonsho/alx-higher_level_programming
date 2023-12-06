3ea24f099a439072a184802b61984aacee75a868
jeonsho <jeonsho1999@gmail.com>
2023-12-06 07:25:02 -0800

    test

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique_numbers = set()
-    
     for number in my_list:
         unique_numbers.add(number)
-    
     result = sum(unique_numbers)
-    
     return result

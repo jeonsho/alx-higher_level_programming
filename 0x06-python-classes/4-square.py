c22458c18866d47c1b4afc36fb8fc77f36f63f1f
jeonsho <jeonsho1999@gmail.com>
2023-12-19 04:21:02 -0800

    test

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """
+    This class represents a square with a private attribute `__size`.
+
+    Attributes:
+    - __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+        - size (int): The size of the square. Defaults to 0.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size attribute.
+
+        Returns:
+        - int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size attribute.
+
+        Parameters:
+        - value (int): The size to be set.
+
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+        - int: The area of the square.
+        """
+        return self.__size ** 2

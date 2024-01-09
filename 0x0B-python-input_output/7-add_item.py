4aa1185455a6b43382b8eae047576d45c2e43fff
jeonsho <jeonsho1999@gmail.com>
2024-01-09 02:47:12 -0800

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Module containing function to save python object in json format"""
+'''
+Write a function that writes an
+Object to a text file, using a
+JSON representation
+'''
+
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Convert `my_obj` to json string and save to `filename`
-
-    Args:
-        my_obj: serializable object to convert to json
-        filename (str): file to save json string to
-    """
-    with open(filename, 'w') as f:
-        json.dump(my_obj, f)
+    '''
+    Writes object to text file using JSON
+    '''
+    if filename is None:
+        return
+    with open(filename, 'w', encoding='utf-8') as f:
+        json_var = json.dump(my_obj, f)

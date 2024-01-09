367e1abcff6d250f68fe8805f6cce1e2ccba36f6
jeonsho <jeonsho1999@gmail.com>
2024-01-09 01:23:59 -0800

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")

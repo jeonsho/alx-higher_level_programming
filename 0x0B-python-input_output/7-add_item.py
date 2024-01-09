be6bbd9a00c6f4caccb9a2583eafbfbc7ce29f9d
jeonsho <jeonsho1999@gmail.com>
2024-01-09 02:50:09 -0800

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-'''
-Write a function that writes an
-Object to a text file, using a
-JSON representation
-'''
-
-import json
+"""This module adds all arguments to a Python list and save them to a file."""
 
 
-def save_to_json_file(my_obj, filename):
-    '''
-    Writes object to text file using JSON
-    '''
-    if filename is None:
-        return
-    with open(filename, 'w', encoding='utf-8') as f:
-        json_var = json.dump(my_obj, f)
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+argv = sys.argv
+
+filename = "add_item.json"
+try:
+    content = load_from_json_file(filename)
+except FileNotFoundError:
+    content = []
+
+for i in range(1, len(argv)):
+    content.append(argv[i])
+
+save_to_json_file(content, filename)

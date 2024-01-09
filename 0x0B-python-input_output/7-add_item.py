dfbe5e25cd2cf0c5a37eed90b9c3166ddc51331a
jeonsho <jeonsho1999@gmail.com>
2024-01-09 02:53:23 -0800

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-from json import dumps
+"""
+    This is a python script that adds all arguments to a Python List.
+    List is then saved to a file.
+"""
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def save_to_json_file(my_obj, filename):
-    with open(filename, 'w') as f:
-        a = dumps(my_obj)
-        f.write(a)
+if __name__ == "__main__":
+
+    filename = "add_item.json"
+
+    try:
+        arg_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        arg_list = []
+
+    for arg in sys.argv[1:]:
+        arg_list.append(arg)
+    save_to_json_file(arg_list, filename)

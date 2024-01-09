3ca3b49559d6d4ba2dce20dcde6d2f06b0a8d093
jeonsho <jeonsho1999@gmail.com>
2024-01-09 02:55:15 -0800

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-"""
-Module 9-add_item
-Contains function that adds and saves to Python obj to JSON file; loads objects
-# run with ./9-add_item.py
-#
-# cat add_item.json ; echo ""
-# expect output: []
-#
-# ./9-add_item.py some random args
-# cat add_item.json ; echo ""
-# expect output: ["some", "random", "args"]
-"""
+"""Module containing function to save python object in json format"""
+import json
 
 
-from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+def save_to_json_file(my_obj, filename):
+    """Convert `my_obj` to json string and save to `filename`
 
-filename = "add_item.json"
-
-try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
-
-save_to_json_file(existing_content + argv[1:], filename)
+    Args:
+        my_obj: serializable object to convert to json
+        filename (str): file to save json string to
+    """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)

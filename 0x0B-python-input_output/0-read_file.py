7872d1025ac660df2087a97d7fc668262c573718
jeonsho <jeonsho1999@gmail.com>
2024-01-09 01:23:20 -0800

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-'''
-Write a function that reads a text file (UTF8)
-and prints it to stdout
-'''
+"""This module defines a text file-reading function"""
 
 
 def read_file(filename=""):

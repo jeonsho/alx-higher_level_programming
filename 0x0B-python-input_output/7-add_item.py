3d377f21163961ad5a5ffa3fb44556cfb5d5a9c4
jeonsho <jeonsho1999@gmail.com>
2024-01-09 01:56:21 -0800

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,20 +4,15 @@
     List is then saved to a file.
 """
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
 
-if __name__ == "__main__":
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
 
-    filename = "add_item.json"
-
-    try:
-        arg_list = load_from_json_file(filename)
-    except FileNotFoundError:
-        arg_list = []
-
-    for arg in sys.argv[1:]:
-        arg_list.append(arg)
-    save_to_json_file(arg_list, filename)
+save_to_json_file(existing_content + argv[1:], filename)
